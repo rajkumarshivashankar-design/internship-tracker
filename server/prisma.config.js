6797c508ca19e8daf4dd9fc17ad57a0794da620b
rajkumarshivashankar-design <rajkumarshivashankar@gmail.com>
2026-09-07 15:32:29 +0530

    Fixed production database config

--- server/prisma.config.js
+++ server/prisma.config.js
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL="postgresql://postgres:root@localhost:5432/internship_tracker"
-  },
+    url: process.env.DATABASE_URL
+},
 });
